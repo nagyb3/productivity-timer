a47e4444ee8c9ac4fd0910ef7fc4aba5bfea88c1
nagyb3 <nagybence2003@gmail.com>
2023-04-29 21:31:40 +0200

    start/stop button with countdown timer

---
+++ src/CountdownTimer.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+function CountdownTimer() {
+    const [timer, setTimer] = useState(1800); // 30 minutes in seconds
+    const [isRunning, setIsRunning] = useState(false);
+
+    useEffect(() => {
+        let interval;
+        if (isRunning) {
+            interval = setInterval(() => {
+                setTimer(prevTimer => prevTimer - 1);
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning]);
+
+    const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
+
+    const handleStart = () => {
+        setIsRunning(!isRunning);
+    };
+
+    function handleMinuteChange() {
+        let hours = 0;
+        let minutes = 0;
+        let newAmount = prompt("");
+        newAmount = [...newAmount]
+        let seconds = parseInt(newAmount.splice(newAmount.length - 2, 2).join(''));
+        if (newAmount.length > 0) {
+            minutes = parseInt(newAmount.splice(newAmount.length - 2, 2).join(''))
+        }
+        if (newAmount.length > 0) {
+            hours = parseInt(newAmount.join(''));
+        }
+        console.log(hours, minutes, seconds)
+        setTimer(hours * 3600 + minutes * 60 + seconds)
+    }
+
+    return (
+        <div>
+            <button onClick={handleStart}>{isRunning ? "STOP" : "START"}</button>
+            <p onClick={handleMinuteChange}>{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</p>
+        </div>
+    );
+}
+
+export default CountdownTimer;
