fb8f03ec2c792ae0a663e0ee1d4c4a08aac94153
nagyb3 <nagybence2003@gmail.com>
2023-06-11 23:11:14 +0200

    Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <ul>
+                <li>made by: <a href="https://www.github.com/nagyb3" target="blank_">Bence Nagy</a></li>
+            </ul>
+        </div>
+    )
+}
