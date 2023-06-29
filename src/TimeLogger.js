79d2e534e36f03d9146704a0a9fcfe19063ac450
nagyb3 <nagybence2003@gmail.com>
2023-06-29 17:42:06 +0200

    show todaysminutes

--- src/TimeLogger.js
+++ src/TimeLogger.js
@@ -2,6 +2,7 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { collection, getDocs, addDoc } from "firebase/firestore"
 import { auth, db } from "./App";
+import dateFormat from "dateformat";
 
 export default function TimeLogger(props) {
 
@@ -10,6 +11,10 @@ export default function TimeLogger(props) {
     const timedataCollectionRef = collection(db, "timedata");
 
     const [newNumber, setNewNumber] = React.useState(null);
+
+    const [showForm, setShowForm] = React.useState(true);
+
+    const [todayMinutes, setTodayMinutes] = React.useState(0);
 
     const logout = async () => {
         await signOut(auth);
@@ -46,24 +51,42 @@ export default function TimeLogger(props) {
                     authorEmail: auth.currentUser.email,
                 });
                 getTimeData();
-                setNewNumber(0);
+                setNewNumber(null);
             } catch (err) {
                 console.log(err)
             }
         }
     }
 
-    console.log(typeof newNumber, newNumber)
+    console.log(timeData[0])
+
+    const getStats = () => {
+        setShowForm(prev => !prev);
+        //get todays minutes for this user
+        let todayMinutesVar = 0;
+        for (let i = 0; i < timeData.length; i++) {
+            if (auth.currentUser.email === timeData[i].authorEmail) {
+                if (dateFormat(timeData[i].submittedAt.toDate(), 'yyyy-mm-dd') === dateFormat(new Date(), 'yyyy-mm-dd')) {
+                    todayMinutesVar += timeData[i].data;
+                }
+            }
+            setTodayMinutes(todayMinutesVar);
+        }
+    }
 
     return (
         <div className="timelogger-container card">
-            <form onSubmit={e => handleSubmit(e)}>
-                <label htmlFor="time">Submit minutes to profile:</label>
-                <input onChange={(e) => setNewNumber(Number(e.target.value))} type="number" name="time" id="time" 
-                placeholder="enter minutes..." value={newNumber}
-                min={0} />
-                <input type="submit" className="submit button" value="SUBMIT"/>
-            </form>
+            <button onClick={getStats}>{ showForm ? "show stats" : "show form"}</button>
+            { showForm ? 
+                <form onSubmit={e => handleSubmit(e)}>
+                    <label htmlFor="time">Submit minutes to profile:</label>
+                    <input onChange={(e) => setNewNumber(Number(e.target.value))} type="number" name="time" id="time" 
+                    placeholder="enter minutes..." value={newNumber}
+                    min={0} />
+                    <input type="submit" className="submit button" value="SUBMIT"/>
+                </form> :
+                <div>Today's minutes: {todayMinutes}</div>
+            }
             <div>
                 <button className="logout" onClick={logout}>LOGOUT</button>
             </div>
